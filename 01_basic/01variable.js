1bc96408900d35da1317ab7b9682e4007a05ce8e
rajansingh-coder <kartikeya7068@gmail.com>
2026-04-03 23:13:46 +0000

    datatype

--- 01_basic/01variable.js
+++ 01_basic/01variable.js
@@ -1,15 +1,15 @@
-const account_Id=123
-let account_Email="rajan@email.com"
-var account_Password="456"
-account_city="Gorakhpur"
+const account_Id = 123
+let account_Email = "rajan@email.com"
+var account_Password = "456"
+account_city = "Gorakhpur"
 let account_state;
 
 
 // account_Id=89  // not  allowed because we cannot change the value  of const further 
 
-account_Email="kartikeya@account_Email.com"
-account_Password="56"
-account_city="Kushinagar"
+account_Email = "kartikeya@account_Email.com"
+account_Password = "56"
+account_city = "Kushinagar"
 
 console.log(account_Id);
 
@@ -18,4 +18,4 @@ prefer not to use var
 because of issue in block scope and functional scope 
 */
 
-console.table([account_Email,account_Id,account_Password,account_city,account_state])
+console.table([account_Email, account_Id, account_Password, account_city, account_state])

c1898d22d35a89bc727dbdfd09cecce78f6dcea9
rajansingh-coder <kartikeya7068@gmail.com>
2026-04-03 22:38:39 +0000

    container config done

---
+++ 01_basic/01variable.js
@@ -0,0 +1,21 @@
+const account_Id=123
+let account_Email="rajan@email.com"
+var account_Password="456"
+account_city="Gorakhpur"
+let account_state;
+
+
+// account_Id=89  // not  allowed because we cannot change the value  of const further 
+
+account_Email="kartikeya@account_Email.com"
+account_Password="56"
+account_city="Kushinagar"
+
+console.log(account_Id);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+console.table([account_Email,account_Id,account_Password,account_city,account_state])

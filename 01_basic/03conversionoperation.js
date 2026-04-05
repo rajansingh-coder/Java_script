fd10848537bde1b380a83e49dcb74913ed002559
rajansingh-coder <kartikeya7068@gmail.com>
2026-04-05 10:31:13 +0000

    typeconversion

--- 01_basic/03conversionoperation.js
+++ 01_basic/03conversionoperation.js
@@ -18,8 +18,8 @@ let ValueInNumber= Number(score)
 
 let isLoggedIn ="Rajan"
 let numberisLoggedIn= Boolean(isLoggedIn)
-console.log(numberisLoggedIn);
-console.log(typeof numberisLoggedIn);
+// console.log(numberisLoggedIn);
+// console.log(typeof numberisLoggedIn);
 
 
 // 1 =>true; 0 => false 
@@ -28,12 +28,54 @@ console.log(typeof numberisLoggedIn);
 
 let somenumber =18
 let stringnumber= String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
 
 
+// ******************* Operations  ********************
 
+let value = 5
+let valueneg = -value
+// console.log(valueneg)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  // iska matlab power hota hai.
+// console.log(2/2);
+// console.log(2%2);
+
+
+str1 ="Hello"
+str2 =" Rajan"
+str3 = str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2 + 3); // agar sttring first hain toh sabhi ko string ki tarah treat kiya jayega 
+// console.log(1+2+"3");   // and agar string last me hai toh pahle type conversion hota hai then string aa jata hai 
+// console.log( (3+3)*8%5);
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1=num2 = num3 =2+72
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+
+// link to Study 
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-toprimitive
 
 
  

33e3c6519681b8ecd1b452cbd5e0e3452c5cb240
rajansingh-coder <kartikeya7068@gmail.com>
2026-04-05 10:00:17 +0000

    conversion of datatypes

---
+++ 01_basic/03conversionoperation.js
@@ -0,0 +1,39 @@
+let score ="Rajan"
+// console.log(typeof score);
+// console.log(typeof (score));  
+
+let ValueInNumber= Number(score)
+// console.log(typeof (ValueInNumber));
+// console.log(ValueInNumber);
+
+
+// let value = null 
+// let ValueNumber= Number(value)
+// console.log(typeof (ValueNumber));
+// console.log(ValueNumber);
+
+// "45"=>33
+// "45abc"=>NaN
+// true =>  1;  false=> 0
+
+let isLoggedIn ="Rajan"
+let numberisLoggedIn= Boolean(isLoggedIn)
+console.log(numberisLoggedIn);
+console.log(typeof numberisLoggedIn);
+
+
+// 1 =>true; 0 => false 
+// "" => false 
+// "Given some value" => true
+
+let somenumber =18
+let stringnumber= String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
+
+ 

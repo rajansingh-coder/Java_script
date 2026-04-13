c8bbab9a05cf0351e4c67bcd61cb6b33205a4d9c
rajansingh-coder <kartikeya7068@gmail.com>
2026-04-13 18:18:37 +0000

    datatypes_summary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn= false 
+const outsideTemp= null
+let userEmail;
+
+
+const id= Symbol('123');
+const anotherid = Symbol('123');
+
+// console.log(id === anotherid);
+
+// const bigNumber= 465699412211n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const hero=["IronMan","Hulk", "SpiderMan"]  //Array
+
+
+// object
+
+let myobj={               
+    name:"Rajan",
+    age: 22
+}
+
+
+// function 
+ const MyFunction= function(){
+    console.log("hello world");
+    
+ }
+
+ console.log(typeof hero);
+ 

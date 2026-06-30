ff95c857b8b006948f555c907dbb5d5e77e38a7d
Rajan Singh <kartikeya7068@gmail.com>
2026-06-30 16:52:55 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,23 @@
+// Function
+
+// In JavaScript, a function is a block of reusable code that performs a specific task. You can call (invoke) a function whenever you need it, instead of writing the same code multiple times.
+
+function saymyname() {
+    console.log("Rajan");
+    console.log("Rajan");
+    console.log("Rajan");
+    console.log("Rajan");
+    console.log("Rajan");
+    console.log("Rajan");
+}
+
+saymyname()
+
+function addTwoNumber(num1, num2)   // when we make function definition at that time the input we are take is called parameter
+{
+    console.log(num1 + num2);
+
+}
+
+addTwoNumber(5, 8)       // while we call the function value is pass is called arguement
+

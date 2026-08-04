f919bcc2ccb8e15dc2c0f004fc96a7f524dc37ec
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-04 22:09:27 +0530

    Interesting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -11,9 +11,9 @@ if (true) {
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);   
-console.log(c);   // it gives the output from the local scope this is the disadvantage of var
+// console.log(c);   // it gives the output from the local scope this is the disadvantage of var
 
 
 // =================================
@@ -32,3 +32,32 @@ function one (){
 }
 
 one()
+
+
+
+if(true){
+    const username ="Rajan"
+    if (username === "Rajan"){
+        const website = " YT";
+        console.log(username + website);
+        
+    }
+    // console.log(website);  
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++++Interesting +++++++++++++++++++++++++++
+
+addone(10)
+function addone(num1){
+    return num1+1;
+}
+
+addtwo(16)                 //  we Cannot access 'addtwo' before initialization
+
+const addtwo = function(num){
+    return num + 2
+}
+

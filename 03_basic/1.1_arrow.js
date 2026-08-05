bc95f4a11865edad95295c1d9967707bdfba629b
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-05 21:34:21 +0530

    More About This

--- 03_basic/1.1_arrow.js
+++ 03_basic/1.1_arrow.js
@@ -15,3 +15,20 @@ user.welcomemessage()
 user.username = "Ram"
 user.welcomemessage()
 console.log(this);         // here we in node environment so this line give empty object if we  are run this in browser its global object is window instead of empty object
+
+
+// when we are running "this" inside the function 
+
+function chai(){
+    let username ="Rajan"
+    // below line gives the output is undefined because it works only with object. It doesn't work with function  
+    console.log(this.username);      
+
+}
+
+chai()
+
+
+
+
+

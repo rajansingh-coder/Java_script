55929fe9cd2ba26ac2a717957bd4947828cf1b4c
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-07 16:48:54 +0530

    IIFE

---
+++ 03_basic/03_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expression(IIFE)
+
+(function unknown(){
+    console.log(`DB  CONNECTED`);
+       
+})();
+
+  ( (name)=> {
+        console.log(`Second time DB CONNECTED `);
+        
+  })('Rajan')

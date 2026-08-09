9655808082e100c18bc325bc44e10e39ab0e6fec
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-10 02:56:09 +0530

    Nested loop

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,57 +1,59 @@
-// // ===================== IF======================
+// ===================== IF======================
 
-// const isUserloggedIn = true
-// const temprature = 30
-// if(temprature >=30){
-//     console.log("It very hot");
-// }
-// else{
-//     console.log("Temperature feels normal");
+const isUserloggedIn = true
+const temprature = 30
+if(temprature >=30){
+    console.log("It very hot");
+}
+else{
+    console.log("Temperature feels normal");
     
-// }
+}
 
-// console.log("This is always execute");
-
-
-// // <, >, <=, >=, ==, !=(not equal to), ===(it return datatype)
-
-// if(2 === "2"){
-//     console.log("executed"); 
-// }
-
-// const score = 200 
-// if(score >= 100){
-//     const batsman = "shatak"
-//     console.log(`batsman make: ${batsman}`); 
-// }
-
-// console.log(`batsman make: ${batsman}`);   // batsman scope within if statement only
-
-// const  balance = 456
-// if(balance > 123){
-//     console.log("rich")
-// }
-
-// // ==================OR==================
-// const  balance = 456
-// if(balance > 123)  console.log("Rich"), console.log("veryy rich");    // we should avoid to write this type of code because its not readable
+console.log("This is always execute");
 
 
-// // ==========================  Nested Condition ==========================
-// balance =  456213
-// if(balance < 5000){
-//     console.log("less than 5000");   
-// } else if (balance < 12000){
-//     console.log("less than 12000"); 
+// <, >, <=, >=, ==, !=(not equal to), ===(it return datatype)
 
-// } else if (balance < 23000){
-//     console.log("less than 23000");
+if(2 === "2"){
+    console.log("executed"); 
+}
+
+const score = 200 
+if(score >= 100){
+    const batsman = "shatak"
+    console.log(`batsman make: ${batsman}`); 
+}
+
+console.log(`batsman make: ${batsman}`);   // batsman scope within if statement only
+
+const  balance = 456
+if(balance > 123){
+    console.log("rich")
+}
+
+// ==================OR==================
+const  balance = 456
+if(balance > 123)  console.log("Rich"), console.log("veryy rich");    // we should avoid to write this type of code because its not readable
+
+
+// ==========================  Nested Condition ==========================
+balance =  456213
+if(balance < 5000){
+    console.log("less than 5000");   
+} else if (balance < 12000){
+    console.log("less than 12000"); 
+
+} else if (balance < 23000){
+    console.log("less than 23000");
     
-// }
-//  else{
-//     console.log("balance is more than u think");
+}
+ else{
+    console.log("balance is more than u think");
     
-//  }
+ }
+
+//  ============= checking multiple condition ==============
 
 const UserloggedIn =  true
 const debitCard = true

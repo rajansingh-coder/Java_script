3066c819fbafc20dab0d1b2b2975699b2aac02da
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-11 10:41:06 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,37 @@
+// ============switch syntax =================
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    default:
+        console.log("Enter between 1 to 12")
+        break;
+}

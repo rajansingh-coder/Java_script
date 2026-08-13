7857f9bc9613248b615aed044935ee04d3b6ef28
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-13 21:55:53 +0530

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,18 @@
+// While LOop
+i = 0
+while(i<=10 ){
+    // console.log(`value is ${i}`);
+    i = i+2     // increment by two
+}
+
+// print the array using while loop
+
+myArray = ["SuperMan", "Wonder Woman","Batman"]
+
+arr = 0
+while(arr < myArray.length){
+    console.log(`DC main hero ${myArray[arr]}`);
+    arr=arr+1
+    
+}
+

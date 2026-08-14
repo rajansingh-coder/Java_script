63096a89497c3d904313b9b62a45aa595ee49e1c
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-14 10:23:16 +0530

    for of Loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,30 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const  arr =  [1,3,4,5,23]
+
+for (const element of arr) {
+    // console.log(element);  
+}
+
+
+const greetings = "hello world!"
+for (const greet of greetings) {
+    // console.log(`char :- ${greet}`);
+    
+}
+
+// Maps
+
+// A Map stores data in key-value pairs: give the output in same order
+
+const  map = new Map()
+map.set('IN', "India")
+map.set('JP', "Japan")
+map.set('UK', "United Kingdom")
+map.set('IN', "India")           // A Map cannot have duplicate keys
+map.set('IN', "Bharat")          // Here is Key are duplicate but the value are difference so the so it replace the "India" to "Bharat"
+
+console.log(map);

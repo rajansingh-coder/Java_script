3697f2d82843826d0dcc316c18293a3f6221e82b
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-16 19:01:45 +0530

    Filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,33 @@
+const language = ["js", "py",  "ruby", "c++", "cotlin"]
+
+// Here if we want to return only  js, py, c++ we cannot return using forEach loop
+// const values =  language.forEach((item) =>  {
+//     console.log(item);
+//     return item
+    
+// } )
+
+// console.log(values);
+
+// Thats why we use filter 
+
+// const myNums =[1,2,3,4,5,6,7,8]
+
+// let  newNums = myNums.filter( (num)=> num < 6)
+// // ================= OR  =====================
+// let  newNums = myNums.filter( (num)=> {
+//     return num < 5
+// })
+// console.log(newNums);
+
+// doing the same thing using 
+
+const myNums =[1,2,3,4,5,6,7,8]
+newNums =[]
+myNums.forEach( (num) =>{
+    if(num < 5){
+        newNums.push(num)
+        
+    }
+} )
+console.log(newNums);

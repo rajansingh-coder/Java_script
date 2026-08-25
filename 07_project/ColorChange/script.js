8bb8206e8669a7a7855ea6577dc14e81cde68c56
rajansingh-coder <kartikeya7068@gmail.com>
2026-08-25 21:45:01 +0530

    Guess the Number

--- 07_project/ColorChange/script.js
+++ 07_project/ColorChange/script.js
@@ -1,26 +1,26 @@
-const  buttons = document.querySelectorAll('.button')
+const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body');
 
-buttons.forEach( function(button){
+buttons.forEach(function (button) {
     console.log(button)
-    button.addEventListener('click',function(event){
+    button.addEventListener('click', function (event) {
         console.log(event)
         console.log(event.target);
-        if(event.target.id ===  'Orange'){
+        if (event.target.id === 'Orange') {
             body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id ===  'Yellow'){
+        if (event.target.id === 'Yellow') {
             body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id ===  'Red'){
+        if (event.target.id === 'Red') {
             body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id ===  'White'){
+        if (event.target.id === 'White') {
             body.style.backgroundColor = event.target.id;
         }
-        if(event.target.id ===  'Blue'){
+        if (event.target.id === 'Blue') {
             body.style.backgroundColor = event.target.id;
         }
-        
+
     })
 })

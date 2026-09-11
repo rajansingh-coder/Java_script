945d5dd8240b6272d49bbefb2b3404ef142af6c0
rajansingh-coder <kartikeya7068@gmail.com>
2026-09-11 23:11:14 +0530

    final  colorchange_project

--- 08_Events/colorChange.js
+++ 08_Events/colorChange.js
@@ -1,26 +1,29 @@
 // Generate the random Color
 
-const randomColor = function(){
+const randomColor = function () {
     const hex = "0123456789ABCDEF"
     let color = "#"
-    for(let i = 0; i<6; i++){
-        color += hex[(Math.floor(Math.random)*16)]
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
     }
     return color;
 
 }
 
 let IntervalID;
-const colorChange =  function(){
-    IntervalID = setInterval(changebgColor,1000);
-function changebgColor(){
-    document.body.style.backgroundColor = randomColor()
+const colorChange = function () {
+    if (!IntervalID) {
+        IntervalID = setInterval(changebgColor, 1000);
+    }
+    function changebgColor() {
+        document.body.style.backgroundColor = randomColor()
+    }
 }
-}
-const stopbgColor = function(){
+const stopbgColor = function () {
     clearInterval(IntervalID);
-
+    IntervalID = null;
 }
 
-document.querySelector('#start').addEventListener('click',colorChange)
-document.querySelector('#stop').addEventListener('click',stopbgColor)
+document.querySelector('#start').addEventListener('click', colorChange)
+document.querySelector('#stop').addEventListener('click', stopbgColor)
+

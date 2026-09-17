cf0538fa61a61b00b90158683be9cc3984c60977
rajansingh-coder <kartikeya7068@gmail.com>
2026-09-17 23:48:10 +0530

    Promises

--- 10_advance/promisesOne.js
+++ 10_advance/promisesOne.js
@@ -1,57 +1,76 @@
-const promiseOne = new  Promise(function(resolve,reject) {
+const promiseOne = new Promise(function (resolve, reject) {
     // Do an async task
     // DB calls,  cryptography,netork
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log("Async  Task Done");
-        resolve()     
+        resolve()
     }, 1000)
 })
 
-promiseOne.then(function(){
+promiseOne.then(function () {
     console.log("promises  Comnsumes");
-    
+
 })
 
-new Promise(function(resolve,reject){
-    setTimeout(function(){
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
         console.log("Async task 2 is done bro");
         resolve()
-    },1000)
-    
-}).then(function(){
+    }, 1000)
+
+}).then(function () {
     console.log("Promises 2 is consume");
-    
+
 })
 
-const promiseThree = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        resolve({username: "rajan", email:"example@123.com"})
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "rajan", email: "example@123.com" })
     });
 })
-promiseThree.then(function(user){
+promiseThree.then(function (user) {
     console.log(user);
-    
+
 })
 
-const promisefour = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let err =false;
-        if(!err){
-            resolve({username:"rajan", password: "12345"})
-        }else{
+const promisefour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let err = false;
+        if (!err) {
+            resolve({ username: "rajan", password: "12345" })
+        } else {
             reject('Something Went Wrong')
         }
-    },1000)
+    }, 1000)
 })
 
-promisefour.then((user)=> {
+promisefour.then((user) => {
     console.log(user);
     return user.username
-}).then((username)=>{
+}).then((username) => {
     console.log(username);
-    
-}).catch(function(err){
+
+}).catch(function (err) {
     console.log(err);
-    
+
+}).finally(() => console.log("The promise either be resolve or either be rejected"));
+
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let err = false;
+        if (!err) {
+            resolve({ username: "JavaScript", password: "12345" })
+        } else {
+            reject('JS Went Wrong')
+        }
+    }, 1000)
 })
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (err) {
+        console.log(err);
+    }
+}

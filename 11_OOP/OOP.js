9d12c88c9567a3190f1f66a47a36c4ed1d53975d
rajansingh-coder <kartikeya7068@gmail.com>
2026-09-19 15:56:20 +0530

    contructor funnction

--- 11_OOP/OOP.js
+++ 11_OOP/OOP.js
@@ -1,4 +1,4 @@
-// Oject Literals
+// Object Literals
 
 const user = {
     username: "Madhav",
@@ -16,4 +16,26 @@ const user = {
 console.log(user.signIn);
 console.log(user.getAllDetails());
 console.log(this);
+
+
+function User(username,password,isLoggedIn){
+    this.username = username
+    this.password = password
+    this.isLoggedIn = isLoggedIn
+
+    return this
+}
+
+// const userOne =User("Rajan", 12345, false)
+// const usertwo =User("Raj", 123, true)
+
+// if we don't use the new key value is overwritten while we console the usertwo 
+// so it not happen  we use "new" keyword and it  is a  constructor which give new instances every time  
+
+const userOne =new  User("Rajan", 12345, false)
+const usertwo =new User("Raj", 123, true)
+
+console.log(userOne);
+console.log(usertwo);
+
 

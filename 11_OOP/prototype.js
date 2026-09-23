f5790742798022baca3fd2bda756eb5ec435f12f
rajansingh-coder <kartikeya7068@gmail.com>
2026-09-23 06:48:29 +0530

    Inheritence

--- 11_OOP/prototype.js
+++ 11_OOP/prototype.js
@@ -22,4 +22,43 @@ Array.prototype.HeySuperHero = function(){
     
 }
 // heroPower.SuperHero()
-MarvelHero.SuperHero()
+// MarvelHero.SuperHero()
+
+
+// Inheritence
+const User = function(){
+    name: "Vrinda"
+    email: "vrinda@gmail.com"
+}
+const Teacher = function(){
+    makevideo:true
+}
+
+const TeachingSupport = function(){
+    isAvailable = false
+}
+
+const TASupport = function(){
+    makeAssignment  : "Interview Question"
+    fulltime: true
+    __proto__:TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern Syntax 
+
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+
+let SecondUser = "Hi Bhai   "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`The true length is : ${this.trim().length}`);
+    
+}
+
+SecondUser.trueLength()
+"Rajan".trueLength()
+"Kartikeya".trueLength()

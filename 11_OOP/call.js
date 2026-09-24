07cb815926afb62814302110cbc9dd40a8bbdab9
rajansingh-coder <kartikeya7068@gmail.com>
2026-09-24 23:35:50 +0530

    call & This

--- 11_OOP/call.js
+++ 11_OOP/call.js
@@ -1,3 +1,16 @@
 function SetuserName(username){
- 
+    // complex db calls 
+    this.username = username
+    console.log("function is called");
+    
 }
+
+function createUser(username,email,passwords){
+    SetuserName.call(this,username)
+
+    this.email  =  email
+    this.passwords =passwords
+}
+
+const hero = new createUser("hero",  "hero@.com", "123456")
+console.log(hero);
